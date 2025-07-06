8f117ae7077e0c4c54008ffa8072c0a41253300a
Matthew Ault <matthew.ault@gmail.com>
2025-07-06 12:22:36 +0100

    run integration tests against db on local host not docker compose

--- tests/integration-tests/conftest.py
+++ tests/integration-tests/conftest.py
@@ -2,7 +2,7 @@ import pytest
 from baconometer.app import create_app
 
 # TODO: use config file instead?
-NEO4J_URI = "bolt://neo4j:7687"
+NEO4J_URI = "bolt://localhost:7687"
 NEO4J_USER = "neo4j"
 NEO4J_PASSWORD = "neo4jtest123"
 
